300ff5b684d1e08a02f885d8a4e10ce05a445254
gingeropolous <gingeropolous@gmail.com>
2015-12-02 00:06:37 -0500

    fixed error in xmr import blockchain

--- xmr_import_blockchain.sh
+++ xmr_import_blockchain.sh
@@ -2,7 +2,8 @@
 #Copies an important monero blockchain to the monerodo data folder
 #assume the name of the blockchain file is monero.blockchain
 
-sudo find / -name 'moneroblockchain.xz' -exec mv {} /.bitmonero/lmdb/  \;
-sudo xz -d -f /.bitmonero/lmdb/moneroblockchain.xz data.mdb
+sudo find / -name 'moneroblockchain.mdb.xz' -exec mv {} /.bitmonero/lmdb/  \;
+sudo xz -d -f /.bitmonero/lmdb/moneroblockchain.mdb.xz 
+sudo mv /.bitmonero/lmdb/moneroblockchain.db /.bitmonero/lmdb/data.mdb
 
 #eventually add some code to check the size of the moved blockchain file to confirm that it was moved
