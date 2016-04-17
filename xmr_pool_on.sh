2642e74169dd984412a389ac47e3f2a5fdc7a159
gingeropolous <gingeropolous@gmail.com>
2016-04-16 22:45:06 -0400

    fix, again, pool start script

--- xmr_pool_on.sh
+++ xmr_pool_on.sh
@@ -3,8 +3,10 @@
 #stop any existing pool instance and copy new autostart file and start pool
 echo "Please enter your password if required"
 sudo service mos_poolnode stop
+sudo service mos_monerowallet stop
 sudo cp $FILEDIR/mos_poolnode.conf /etc/init/mos_poolnode.conf
 sudo cp $FILEDIR/mos_monerowallet.conf /etc/init/mos_monerowallet.conf
+sudo service mos_monerowallet start
 sudo service mos_poolnode start
 echo "The pool has been turned on"
 echo "Press enter to continue"
