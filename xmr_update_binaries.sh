b434e59076a4e4a064de91fdf965ef1245258964
gingeropolous <gingeropolous@gmail.com>
2016-09-20 15:35:39 -0400

    change updage binaries to use new binary names and create forever list of old bins and name new bin folders with datestamps

--- xmr_update_binaries.sh
+++ xmr_update_binaries.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Monero Update script
 
+now=$(date +"%m_%d_%Y")
+
 cd /home/bob/monero_files/
-mkdir monero_core_09
-cd monero_core_09
+mkdir monero_$now
+cd monero_$now
 
 wget https://downloads.getmonero.org/linux64
 tar -xjvf linux64
@@ -13,11 +15,10 @@ tar -xjvf linux64
 export running=$(service mos_bitmonero status)
 export mos_service="mos_bitmonero"
 
-cd /home/bob/monerodo/
-./service_off.sh
+/home/bob/monerodo/service_off.sh
 
-sudo cp /home/bob/monero_files/monero_core_09/bitmonerod /bin
-sudo cp /home/bob/monero_files/monero_core_09/simplewallet /bin
+sudo cp monerod /bin
+sudo cp monero-wallet-cli /bin
 
 echo "You'll have to turn monero core on again in the settings menu. Press enter to continue"
 read goback
